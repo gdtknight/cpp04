39357de734e4106e3fa90d3eb5335ec767e238bb
yoshin <yoshin@student.42gyeongsan.kr>
2026-01-18 20:55:29 +0900

    Fix(ex03): Initialize all inventory slots in Character's copy constructor
    The Character copy constructor did not initialize inventory slots to NULL if the source's slots were NULL, leading to uninitialized pointers. This commit fixes the bug by adding an else block to explicitly set the destination slot to NULL.

--- ex03/Character.cpp
+++ ex03/Character.cpp
@@ -6,31 +6,43 @@
 /*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 13:47:01 by yoshin            #+#    #+#             */
-/*   Updated: 2026/01/16 14:16:39 by yoshin           ###   ########.fr       */
+/*   Updated: 2026/01/18 20:39:13 by yoshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Character.hpp"
 #include "ICharacter.hpp"
 
+#include <iostream>
+
 Character::Character(void) : name("unknown") {
+  std::cout << "Character default constructor called." << std::endl;
+
   for (int i = 0; i < 4; i++)
     (this->inventory)[i] = NULL;
 }
 
 Character::Character(const std::string &name) : name(name) {
+  std::cout << "Character string constructor called." << std::endl;
+
   for (int i = 0; i < 4; i++)
     (this->inventory)[i] = NULL;
 }
 
 Character::Character(const Character &src) : name(src.name) {
+  std::cout << "Character copy constructor called." << std::endl;
+
   for (int i = 0; i < 4; i++) {
     if (src.inventory[i])
       (this->inventory)[i] = src.inventory[i]->clone();
+    else
+      (this->inventory)[i] = NULL;
   }
 }
 
 Character::~Character(void) {
+  std::cout << "Character destructor called." << std::endl;
+
   for (int i = 0; i < 4; i++) {
     if ((this->inventory)[i])
       delete (this->inventory)[i];
@@ -38,6 +50,8 @@ Character::~Character(void) {
 }
 
 Character &Character::operator=(const Character &rhs) {
+  std::cout << "Character assignation operator called." << std::endl;
+
   if (this != &rhs) {
     this->name = rhs.name;
     for (int i = 0; i < 4; i++) {
@@ -49,6 +63,7 @@ Character &Character::operator=(const Character &rhs) {
         (this->inventory)[i] = NULL;
     }
   }
+
   return (*this);
 }
 
