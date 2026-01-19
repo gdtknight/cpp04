2ac96007cebdd89e5474e3b3bca4ec3af053eb18
yoshin <yoshin@student.42gyeongsan.kr>
2026-01-19 15:07:25 +0900

    chore(ex03): add AMateria lifecycle logging

--- ex03/Amateria.cpp
+++ ex03/Amateria.cpp
@@ -6,32 +6,43 @@
 /*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 13:48:10 by yoshin            #+#    #+#             */
-/*   Updated: 2026/01/16 11:18:21 by yoshin           ###   ########.fr       */
+/*   Updated: 2026/01/18 21:25:13 by yoshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Amateria.hpp"
 #include "ICharacter.hpp"
+
 #include <iostream>
 
-AMateria::AMateria(void) : type("unknown") {}
+AMateria::AMateria(void) : type("unknown") {
+  std::cout << "AMateria default constructor called" << std::endl;
+}
 
-AMateria::AMateria(std::string const &type) : type(type) {}
+AMateria::AMateria(std::string const &type) : type(type) {
+  std::cout << "AMateria string constructor called" << std::endl;
+}
 
-AMateria::AMateria(AMateria const &src) : type(src.type) {}
+AMateria::AMateria(AMateria const &src) : type(src.type) {
+  std::cout << "AMateria copy constructor called" << std::endl;
+}
 
-AMateria::~AMateria() {}
+AMateria::~AMateria() {
+  std::cout << "AMateria destructor called" << std::endl;
+}
 
 AMateria &AMateria::operator=(AMateria const &rhs) {
+  std::cout << "AMateria assignation operator called" << std::endl;
+
   if (this != &rhs)
     type = rhs.type;
+
   return (*this);
 }
 
 std::string const &AMateria::getType() const { return (type); }
 
 void AMateria::use(ICharacter &target) {
-  (void)target;
   std::cout << "* generic materia used on " << "a character("
             << target.getName() << ")" << " *" << std::endl;
 }
