dd180de8c81d4d62925697ed932501c7a0c1b368
yoshin <yoshin@student.42gyeongsan.kr>
2026-01-19 15:07:20 +0900

    chore(ex02): add Dog lifecycle logging

--- ex02/Dog.cpp
+++ ex02/Dog.cpp
@@ -6,27 +6,41 @@
 /*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 18:13:48 by yoshin            #+#    #+#             */
-/*   Updated: 2026/01/06 19:35:57 by yoshin           ###   ########.fr       */
+/*   Updated: 2026/01/18 21:25:40 by yoshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Dog.hpp"
 #include "Brain.hpp"
+
 #include <iostream>
 
-Dog::Dog(void) : AAnimal("Dog") { brain = new Brain(); }
+Dog::Dog(void) : AAnimal("Dog") {
+  std::cout << "Dog default constructor called" << std::endl;
 
-Dog::Dog(const Dog &other) : AAnimal(other) {
+  brain = new Brain();
+}
+
+Dog::Dog(Dog const &other) : AAnimal(other) {
+  std::cout << "Dog copy constructor called" << std::endl;
+
   brain = new Brain(*(other.brain));
 }
 
-Dog::~Dog(void) { delete brain; }
+Dog::~Dog(void) {
+  std::cout << "Dog destructor called" << std::endl;
+
+  delete brain;
+}
 
 Dog &Dog::operator=(const Dog &other) {
+  std::cout << "Dog assignation operator called" << std::endl;
+
   if (this != &other) {
     AAnimal::operator=(other);
-    *brain = *(other.brain); // Brain의 operator= 사용
+    (*brain) = *(other.brain); // Brain의 operator= 사용
   }
+
   return (*this);
 }
 
