1a85f512667288aec758e85ca46f1434ede2a738
yoshin <yoshin@student.42gyeongsan.kr>
2026-01-19 15:07:07 +0900

    chore(ex02): add Brain lifecycle logging

--- ex02/Brain.cpp
+++ ex02/Brain.cpp
@@ -6,34 +6,46 @@
 /*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 19:09:22 by yoshin            #+#    #+#             */
-/*   Updated: 2026/01/06 19:28:21 by yoshin           ###   ########.fr       */
+/*   Updated: 2026/01/18 21:23:08 by yoshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "Brain.hpp"
+
+#include <iostream>
 #include <stdexcept>
 
 Brain::Brain(void) {
+  std::cout << "Brain default constructor called" << std::endl;
+
   for (int idx = 0; idx < 100; idx++)
     ideas[idx] = "nothing";
 }
 
-Brain::Brain(const Brain &other) {
+Brain::Brain(Brain const &other) {
+  std::cout << "Brain copy constructor called" << std::endl;
+
   for (int idx = 0; idx < 100; idx++)
     ideas[idx] = other.ideas[idx];
 }
-Brain::~Brain(void) {}
+Brain::~Brain(void) { std::cout << "Brain destructor called" << std::endl; }
 
-Brain &Brain::operator=(const Brain &other) {
+Brain &Brain::operator=(Brain const &other) {
+  std::cout << "Brain assignation operator called" << std::endl;
+
   if (this != &other) {
     for (int idx = 0; idx < 100; idx++)
       ideas[idx] = other.ideas[idx];
   }
+
   return (*this);
 }
 
 const std::string &Brain::getIdeaAt(int idx) const {
+
   if (idx < 0 || idx >= 100)
     throw std::out_of_range("Brain index");
+
   return (ideas[idx]);
 }
 
