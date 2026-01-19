4413eb942f5df09cd2699ec4c6cc48ccb69b871d
yoshin <yoshin@student.42gyeongsan.kr>
2026-01-19 15:07:38 +0900

    chore(ex03): add Ice lifecycle logging

--- ex03/Ice.cpp
+++ ex03/Ice.cpp
@@ -6,23 +6,31 @@
 /*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 13:48:48 by yoshin            #+#    #+#             */
-/*   Updated: 2026/01/15 11:07:21 by yoshin           ###   ########.fr       */
+/*   Updated: 2026/01/18 21:15:53 by yoshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Ice.hpp"
 #include "ICharacter.hpp"
+
 #include <iostream>
 
-Ice::Ice() : AMateria("ice") {}
+Ice::Ice() : AMateria("ice") {
+  std::cout << "Ice default constructor called" << std::endl;
+}
 
-Ice::Ice(Ice const &src) : AMateria(src) {}
+Ice::Ice(Ice const &src) : AMateria(src) {
+  std::cout << "Ice copy constructor called" << std::endl;
+}
 
-Ice::~Ice() {}
+Ice::~Ice() { std::cout << "Ice destructor called" << std::endl; }
 
 Ice &Ice::operator=(Ice const &rhs) {
+  std::cout << "Ice assignation operator called" << std::endl;
+
   if (this != &rhs)
     AMateria::operator=(rhs);
+
   return (*this);
 }
 
